89b70ec482a0d38e9889cdef381a48119768965d
Gopicrazy <49509975+Gopicrazy@users.noreply.github.com>
2019-04-25 12:00:49 +0530

    Add files via upload

---
+++ cont.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+$_SESSION['link']=$_POST['link'];
+?>
+<html>
+<form action="note.php" method="post">
+Mobile no:<input type="tel" name="mono"/><br>
+Email:<input type="email" name="mail"/><br>
+<input type="submit" name="submit" value="ok"/>
+</form>
+</html>
